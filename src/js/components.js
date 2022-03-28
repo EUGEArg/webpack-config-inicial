9d9dba2072864487645b59d4ad576c058eb86e0a
EUGEarg <eugeniarivarossa@gmail.com>
2022-03-27 21:09:37 -0300

    commit

--- src/js/components.js
+++ src/js/components.js
@@ -3,7 +3,7 @@ import '../css/components.css';
 
 export const saludar = (nombre) => {
 
-    console.log('Creando Etiqueta h1...');
+    // console.log('Creando Etiqueta h1...');
 
     const h1 = document.createElement('h1');
     h1.innerText = `Hola ${nombre}`
